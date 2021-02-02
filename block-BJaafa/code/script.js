4098408c5b88519678de59dd0d9a7db4c964f310
jvegiraju <raju.vegiraju18@gmail.com>
2021-02-02 01:35:06 -0500

    Added Search bar

--- block-BJaafa/code/script.js
+++ block-BJaafa/code/script.js
@@ -1,3 +1,41 @@
+const weather = [
+  {
+    name: "Thunderstorm",
+    img: '<i class="fas fa-poo-storm"></i>',
+  },
+  {
+    name: "Drizzle",
+    img: '<i class="fas fa-cloud-rain"></i>',
+  },
+  {
+    name: "Rain",
+    img: '<i class="fas fa-cloud-showers-heavy"></i>',
+  },
+  {
+    name: "	Snow",
+    img: '<i class="fas fa-snowflake"></i>',
+  },
+  {
+    name: "Clear",
+    img: '<i class="fas fa-sun"></i>',
+  },
+  {
+    name: "Clouds",
+    img: '<i class="fas fa-cloud"></i>',
+  },
+];
+
+var weatherdata;
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly,alerts&appid=eb7694a0b7349579e404ebd37478b13f"
+)
+  .then((response) => response.json())
+  .then((data) => (weatherdata = data))
+  .then(console.log);
+
+console.log(weatherdata);
+
 function currentTime() {
   var date = new Date(); /* creating object of Date class */
   var hour = date.getHours();
@@ -27,6 +65,24 @@ function currentTime() {
   }, 1000);
 }
 
+function weatherUi(weatherdata) {
+  console.log(weatherdata);
+  const div = document.createElement("div");
+  const currentdata = weatherdata.current.weather.main;
+  const h1 = document.createElement("h1");
+  const h2 = document.createElement("h3");
+  const span = document.createElement("span");
+  weather.filter((w) => {
+    if (w.name === currentdata) {
+      span.innerHTML = w.img;
+    }
+  });
+  h1.innerHTML = "Today";
+  h2.innerHTML = weatherdata.current.temp;
+  div.append(span, h1, h2);
+  document.querySelector(".weather").append(div);
+}
+
 function updateTime(k) {
   if (k < 10) {
     return "0" + k;
@@ -35,4 +91,8 @@ function updateTime(k) {
   }
 }
 
+weatherUi(weatherdata);
+
 currentTime();
+
+console.log(weatherdata);
