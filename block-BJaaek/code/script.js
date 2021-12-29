0490e82f1874062b5db9486b96dfbf66c6d26f36
Janakiramaraju Vegiraju <Janakiramaraju.X.Vegiraju@kp.org>
2021-12-29 01:12:53 -0500

    random color generator

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,18 +1,18 @@
-const box1 = document.querySelector(".first");
-const box2 = document.querySelector(".second");
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
 
-box1.addEventListener("click", setRandomColor);
-box2.addEventListener("mousemove", setRandomColor);
+box1.addEventListener("click",setRandomColor);
+box2.addEventListener("mousemove",setRandomColor);
 
-function setRandomColor() {
+function setRandomColor(){
   this.style.backgroundColor = getRandomColor();
 }
 
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getRandomColor(){
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for(let i=0;i<6;i++){
+    color+= letters[Math.floor(Math.random()*16)]
   }
   return color;
 }
